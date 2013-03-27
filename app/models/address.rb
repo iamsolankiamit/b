db26347f086d0b94acdcb2e92cb58c5f6911089e
onerakeshone <onerakeshone@gmail.com>
2013-03-27 10:32:50 +0530

    offers form done

--- app/models/address.rb
+++
@@ -1,4 +0,0 @@
-class Address < ActiveRecord::Base
-  attr_accessible :address_line_2, :city, :country_iso, :lat, :lng, :street, :zip
-  belongs_to :offer
-end
--- app/models/detail.rb
+++
@@ -1,4 +0,0 @@
-class Detail < ActiveRecord::Base
-  attr_accessible :accomodation, :alternate, :bathrooms, :bed_type, :bedroom, :beds, :email, :phone, :property_type, :size
-  belongs_to :offer
-end
