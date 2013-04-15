4a4bf6dd13a15540bc3c2c68fcb849751b00e6d2
onerakeshone <onerakeshone@gmail.com>
2013-04-15 15:43:55 +0530

    changed assets location to proper fog directory

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,7 +8,7 @@ Roomnhouse::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  config.action_controller.asset_host =  "//roomnhouse.s3.amazonaws.com"
+  config.action_controller.asset_host = "//#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
