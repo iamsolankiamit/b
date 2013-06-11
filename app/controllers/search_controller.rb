b3cb90ca51c2403b4c2c9898a73aa81e822f2ff4
onerakeshone <onerakeshone@gmail.com>
2013-06-11 19:49:08 +0530

    search, login, register views

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,8 @@
 class SearchController < ApplicationController
   def index
-  	@offers =Offer.search(params[:search])
+  	@offers =Offer.search(params[:destination])
+
+  	#@photos = Photo.find(:all,:conditions => ['offer_id = ?', @offers.first.id])
+  	#@translations = Translation.find(:all,:conditions => ['offer_id = ? and locale="en"', @offers.first.id])
   end
 end
