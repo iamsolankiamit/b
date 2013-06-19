896abee236c3af7296a916b4598abe768db0e093
onerakeshone <onerakeshone@gmail.com>
2013-06-20 01:38:33 +0530

    removed listings limit on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-  	@offers = Offer.where(:visiblity => true, :is_verified => true).order(:created_at).limit(6)
+  	@offers = Offer.where(:visiblity => true, :is_verified => true).order(:created_at)
   end
 end
