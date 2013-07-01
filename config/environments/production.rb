8b5a58251776700217d54b4b01344317fcfd7722
onerakeshone <onerakeshone@gmail.com>
2013-07-02 01:23:09 +0530

    added compressed gziped optimisation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Roomnhouse::Application.configure do
   config.action_controller.asset_host = "//#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -21,7 +21,7 @@ Roomnhouse::Application.configure do
   config.assets.css_compressor = :yui
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
