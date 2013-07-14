0a8d7522cbcbce1c8df7cd9eae333a2ac1abf381
Amit Solanki <onerakeshone@gmail.com>
2013-07-14 21:57:55 +0530

    bootstrap-sass added

--- config/application.rb
+++ config/application.rb
@@ -55,7 +55,7 @@ module Roomnhouse
 
     # Enable the asset pipeline
     config.assets.enabled = true
-   # config.assets.digest = true
+    config.assets.digest = true
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
