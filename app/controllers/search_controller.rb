b5b8093edc2dc1cbb6047ec22e3d812bfda58db9
Amit Solanki <onerakeshone@gmail.com>
2013-09-14 09:34:44 +0530

    auto distance calculations for search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,12 @@
 class SearchController < ApplicationController
   def index
-  	@offers =Offer.near(params[:destination], 50, :order => :distance).where(:is_verified => true)
+    result = Geocoder.search(params[:destination]).map(&:geometry)
+    north_east_lat = result[0]["viewport"]["northeast"]["lat"]
+    north_east_lng = result[0]["viewport"]["northeast"]["lng"]
+    south_west_lat = result[0]["viewport"]["southwest"]["lat"]
+    south_west_lng = result[0]["viewport"]["southwest"]["lng"]
+    distance = Geocoder::Calculations.distance_between([north_east_lat,north_east_lng], [south_west_lat,south_west_lng])
+  	@offers =Offer.near(params[:destination], distance, :order => :distance).where(:is_verified => true)
 
   	#@photos = Photo.find(:all,:conditions => ['offer_id = ?', @offers.first.id])
   	#@translations = Translation.find(:all,:conditions => ['offer_id = ? and locale="en"', @offers.first.id])
