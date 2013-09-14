d9a20e05b4b3c5dd706e702fa26bf63b3d6248ad
Amit Solanki <onerakeshone@gmail.com>
2013-09-14 08:06:10 +0530

    added basic geo search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
-  	@offers =Offer.search(params[:destination])
+  	@offers =Offer.near(params[:destination], 10, :order => :distance)
 
   	#@photos = Photo.find(:all,:conditions => ['offer_id = ?', @offers.first.id])
   	#@translations = Translation.find(:all,:conditions => ['offer_id = ? and locale="en"', @offers.first.id])
