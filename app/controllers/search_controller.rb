03efb8b7fd505cb658c09c3c2b1c6f7c1826793d
Amit Solanki <onerakeshone@gmail.com>
2013-09-14 09:40:04 +0530

    radius cacuation error sorted

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,7 +5,7 @@ class SearchController < ApplicationController
     north_east_lng = result[0]["viewport"]["northeast"]["lng"]
     south_west_lat = result[0]["viewport"]["southwest"]["lat"]
     south_west_lng = result[0]["viewport"]["southwest"]["lng"]
-    distance = Geocoder::Calculations.distance_between([north_east_lat,north_east_lng], [south_west_lat,south_west_lng])
+    distance = Geocoder::Calculations.distance_between([north_east_lat,north_east_lng], [south_west_lat,south_west_lng])/2
   	@offers =Offer.near(params[:destination], distance, :order => :distance).where(:is_verified => true)
 
   	#@photos = Photo.find(:all,:conditions => ['offer_id = ?', @offers.first.id])
