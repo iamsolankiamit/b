db9c02c8e42c5ec4ac31eba2db4e3fb41f418d40
Amit Solanki <onerakeshone@gmail.com>
2013-09-14 08:10:35 +0530

    ..

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
-  	@offers =Offer.near(params[:destination], 10, :order => :distance)
+  	@offers =Offer.near(params[:destination], 10, :order => :distance).where(:is_verified => true)
 
   	#@photos = Photo.find(:all,:conditions => ['offer_id = ?', @offers.first.id])
   	#@translations = Translation.find(:all,:conditions => ['offer_id = ? and locale="en"', @offers.first.id])
