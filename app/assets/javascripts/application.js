9377e426ddccd4964c631e347577ce7d67ea4f2b
Amit Solanki <onerakeshone@gmail.com>
2013-10-03 17:50:17 +0530

    ...

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 //= require jquery
-//= require jquery.ui.all
+//= require jquery.ui.datepicker
 //= require bootstrap.min.js
 //= require jquery_ujs
 //= require s3_direct_upload
