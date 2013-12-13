1adf45030f05ae3ed387d12e55fc411df40a0764
Amit Solanki <onerakeshone@gmail.com>
2013-12-13 12:17:32 +0530

    made Procfile changes for passenger

--- app/mailers/trip_registered.rb
+++ app/mailers/trip_registered.rb
@@ -1,6 +1,6 @@
 class UserMailer < ActionMailer::Base
   default from: "notifications@roomnhouse.com"
   def trip_registered(user)
-    mail(:to => user.email, :subject => "booking done")
+    mail(:to => user.email, :subject => "Booking complete")
   end
 end
