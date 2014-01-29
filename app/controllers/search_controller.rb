86d946b3e737e2b4dd58db148cc72ee7c1591c47
Amit Solanki <onerakeshone@gmail.com>
2014-01-29 11:47:35 +0530

    checkin checkout dates are now stored in cookies

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,8 @@ class SearchController < ApplicationController
   def index
     @offers =Offer.near(params[:destination], 50, :order => :distance).where(:is_verified => true)
     #   @json =@offers.to_gmaps4rails
+    cookies[:checkin] = params[:checkin] unless params[:checkin].nil?
+    cookies[:checkout] = params[:checkout] unless params[:checkout].nil?
     @hash = Gmaps4rails.build_markers(@offers) do |offers, marker|
       marker.lat offers.confidential_lat
       marker.lng offers.confidential_lng
