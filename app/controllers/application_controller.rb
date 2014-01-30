e54f8a7b26ba0470eca792807893dedfe111ee16
Amit Solanki <onerakeshone@gmail.com>
2014-01-30 15:08:39 +0530

    root path set as redirect after sign ing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   end
 
   def redirect_back_or_default(default)
-    session[:passthru] || default_path
+    session[:passthru] || root_path
   end
 
   def after_sign_in_path_for(resource_or_scope)
