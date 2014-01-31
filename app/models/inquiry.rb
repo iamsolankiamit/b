9cb6eeeba5f1ed70350579c23db0b4cc0dfb5209
Amit Solanki <onerakeshone@gmail.com>
2014-01-31 18:11:05 +0530

    rake precompile dome

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,7 +1,7 @@
 class Inquiry < ActiveRecord::Base
   attr_accessible :check_in, :check_out, :guest_count, :offer_id, :receiver_id, :sender_id , :messages_attributes
   has_many :messages
-  accepts_nested_attributes_for :amenity
+  accepts_nested_attributes_for :message
 
   belongs_to :sender , class_name: "User", foreign_key: "sender_id"
   belongs_to :receiver , class_name: "User", foreign_key: "receiver_id"
