64eff3b81b7d64067b9a1babc853edbc2480c249
Amit Solanki <onerakeshone@gmail.com>
2014-02-10 17:15:06 +0530

    reset to non watermarker photo

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,7 +4,7 @@ class Photo < ActiveRecord::Base
   has_attached_file :image,
     :processors => [:thumbnail, :watermark],
     :styles => {
-      :slider => { geometry: "640x420", watermark_path: "#{Rails.root}/app/assets/images/roomnhouse-grey.png" },
+      :slider => "640x420",
       :medium => "300x300#",
       :searchimg => "235x185#",
       :thumb => "100x100#",
