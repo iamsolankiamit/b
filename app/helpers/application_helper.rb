f6c6a1f0eefe21854b9acec1ebbf92aa6ec60cd2
Amit Solanki <amit.fash@gmail.com>
2014-03-04 16:40:27 +0530

    Bug solved. New user wasn't created instead the host user was assumed, and edit was opened instead of new.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,15 @@
 module ApplicationHelper
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 
   def flash_class(level)
     case level
@@ -8,18 +19,18 @@ module ApplicationHelper
     when :alert then "alert alert-error"
     end
   end
-  
-	def link_to_remove_fields(name, f, options = {})
+
+  def link_to_remove_fields(name, f, options = {})
     f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)", options)
   end
- 
+
   def link_to_add_fields(name, f, association, options = {})
     new_object = f.object.class.reflect_on_association(association).klass.new
     fields = f.fields_for(association, new_object, :child_index => "new_#{ association }") do |builder|
       render(association.to_s.singularize + "_fields", :f => builder)
     end
- 
+
     link_to_function(name, "add_fields(this, \"#{ association }\", \"#{ escape_javascript(fields) }\")", options)
   end
-  
+
 end
