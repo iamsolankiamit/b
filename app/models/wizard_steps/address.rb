684fdd635ff40759c904f477e845f41f728a7b4b
Amit Solanki <amit.fash@gmail.com>
2014-03-27 13:20:41 +0530

    ...

--- app/models/wizard_steps/address.rb
+++ app/models/wizard_steps/address.rb
@@ -1,5 +1,4 @@
 class Offer::Address < Offer
   validates :address_addon, :street, :street_no, :city, :country_code_iso, :presence => true
   validates_numericality_of :zip
-  after_initialize initialize_from_user
 end
