b085a56cee08054cb517199aa042b2f174b3da42
Amit Solanki <amit.fash@gmail.com>
2014-04-26 07:18:53 +0530

    change limits on no of popular apatments on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-		@offers = Offer.where(visiblity: true, is_verified: true).order("RANDOM()").limit(12).includes(:translations,:photos)
-    @recently_viewed_offers = Offer.where(id: session[:recently_viewed_offers]).limit(4) if session[:recently_viewed_offers]
+		@offers = Offer.where(visiblity: true, is_verified: true).order("RANDOM()").limit(5).includes(:translations,:photos)
+    @recently_viewed_offers = Offer.where(id: session[:recently_viewed_offers]).limit(5) if session[:recently_viewed_offers]
   end
 end
