e13a97645c7906b8bd79957af0dbfd6289c370aa
Amit Solanki <amit.fash@gmail.com>
2014-05-18 21:38:31 +0530

    ..

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,11 +2,7 @@ class TripsController < ApplicationController
   before_filter :authenticate_user!
   load_and_authorize_resource
   def index
-    if current_user
       @trips = Trip.where(guest_id: current_user.id)
-    else
-      redirect_to offers_url
-    end
   end
 
   def show
