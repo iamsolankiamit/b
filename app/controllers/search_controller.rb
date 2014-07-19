245cbb2ee541c2f2e7b4839687a1b61f37185639
Nishant <nishant@nishant-300E4C-300E5C-300E7C.(none)>
2014-07-19 20:12:27 +0530

    filter for bedtype,guest,amenities

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,9 +3,10 @@ class SearchController < ApplicationController
 
  		@offers ||= find_offers
 		
-		@price_min = @offers.minimum(:nightly_rate_amount)
- 		@price_max = @offers.maximum(:nightly_rate_amount)
+		@price_min = Offer.near(params[:destination], 50).minimum(:nightly_rate_amount)
+ 		@price_max = Offer.near(params[:destination], 50).maximum(:nightly_rate_amount)
 
+ 		@offers = find_amenities if params[:amenities]
  		# @offers = @offers.order('search_rank DESC')
  		cookies[:checkin] = params[:checkin] unless params[:checkin].nil?
  		cookies[:checkout] = params[:checkout] unless params[:checkout].nil?
@@ -17,7 +18,14 @@ class SearchController < ApplicationController
 
  		price_min = params[:price_min].to_i
  		price_max = params[:price_max].to_i
- 		redirect_to search_path(destination,guests: params[:guests],checkin: params[:checkin], checkout: params[:checkout], bedrooms: params[:bedrooms], price_min: params[:price_min] , price_max: params[:price_max] , bed_count: params[:bed_count])
+
+ 		if params[:amenities]
+ 		redirect_to search_path(destination,guests: params[:guests],checkin: params[:checkin], checkout: params[:checkout], bedrooms: params[:bedrooms], price_min: params[:price_min] , price_max: params[:price_max] , bed_count: params[:bed_count] , bed_type: params[:bed_type] , max_guest_count: params[:max_guest_count] , amenities: { breakfast: params[:amenities][:breakfast] ,air_conditioning: params[:amenities][:air_conditioning] , smoking_allowed: params[:amenities][:smoking_allowed], cable_tv: params[:amenities][:cable_tv] , internet: params[:amenities][:internet] , kitchen: params[:amenities][:kitchen]  })
+
+else
+ 		redirect_to search_path(destination,guests: params[:guests],checkin: params[:checkin], checkout: params[:checkout], bedrooms: params[:bedrooms], price_min: params[:price_min] , price_max: params[:price_max] , bed_count: params[:bed_count] , bed_type: params[:bed_type] , max_guest_count: params[:max_guest_count] )
+end
+
  	end
 
 
@@ -29,7 +37,13 @@ class SearchController < ApplicationController
  	private
 
  	def find_offers
- 		Offer.near(params[:destination], 50, :order => :distance).where(conditions).order('search_rank DESC')
+ 		Offer.near(params[:destination], 50).where(conditions).order('search_rank DESC')
+ 	end
+
+ 	def find_amenities
+ 		@offers = @offers.joins(:amenity).where(amenities_condition)
+ 		ActiveRecord::Associations::Preloader.new(@offers, :amenity).run
+ 		@offers
  	end
 
  	def conditions
@@ -70,5 +84,42 @@ class SearchController < ApplicationController
  		[' bed_count >= ?',params[:bed_count]] if params[:bed_count]
   	end
 
+  	def max_guest_conditions
+  		[' max_guest_count >= ?' ,params[:max_guest_count]] if params[:max_guest_count] 
+  	end
+
+  	def bedtype_conditions
+  		[' bed_type LIKE ?' ,params[:bed_type]] if params[:bed_type]
+  	end
+
+  	def amenities_condition
+ 		functions = private_methods(false).grep(/_amenity_condition$/).map { |m| send(m) }.compact
+ 		conditions_parts_amen = functions
+ 		x=conditions_parts_amen.map { |condition| condition.first }
+ 		y=functions.map{ |condition| condition[-1..1] }.flatten
+[x.join('AND'),*y]         # * represents the block as intake
+  	end
+
+  	def breakfast_amenity_condition
+  		[' amenities.breakfast = ? ', true ] if (params[:amenities][:breakfast] == "true")  
+  	end
+  	def air_conditioning_amenity_condition
+  		[' amenities.air_conditioning = ? ', true ] if (params[:amenities][:air_conditioning] == "true")  
+  	end
+  	def smoking_allowed_amenity_condition
+  		[' amenities.smoking_allowed = ? ', true ] if (params[:amenities][:smoking_allowed] == "true")  
+  	end
+  	
+  	def cable_tv_amenity_condition
+  		[' amenities.cable_tv = ? ', true ] if (params[:amenities][:cable_tv] == "true")  
+  	end
+
+  	def Internet_amenity_condition
+  		[' amenities.internet = ? ', true ] if (params[:amenities][:internet] == "true")  
+  	end
+
+  	def Kitchen_amenity_condition
+  		[' amenities.kitchen = ? ', true ] if (params[:amenities][:kitchen] == "true")  
+  	end
 
 end
