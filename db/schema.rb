96e7f7f1bdfaf382bb58cb95a7f5a1df885306b6
Amit Solanki <amit.fash@gmail.com>
2014-08-07 05:38:16 +0530

    made rake work removed Unit test folders, since we dont use them

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140806075203) do
+ActiveRecord::Schema.define(:version => 20140726102648) do
 
   create_table "amenities", :force => true do |t|
     t.string   "offer_id",                :limit => 6
@@ -221,6 +221,7 @@ ActiveRecord::Schema.define(:version => 20140806075203) do
     t.integer  "booking_id"
     t.integer  "trip_id"
     t.integer  "guest_id"
+    t.integer  "host_id"
     t.integer  "transfer_no"
     t.float    "commission"
     t.string   "status"
