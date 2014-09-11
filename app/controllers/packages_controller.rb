679179aa0a8d957107e88a7cfd46ca292525c1a5
Amit Solanki <amit.fash@gmail.com>
2014-09-11 13:10:44 +0530

    ..

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -2,7 +2,22 @@ class PackagesController < ApplicationController
   # GET /packages
   # GET /packages.json
   def index
-    @packages = Package.all
+    @package1 = Package.find(3)
+    @package1offer = Offer.find("wx1k7g")
+    @package2 = Package.find(4)
+    @package2offer = Offer.find("pwpjzg")
+    @package3 = Package.find(5)
+    @package3offer = Offer.find("nt7ggg")
+    @package4 = Package.find(6)
+    @package4offer = Offer.find("p7mfvg")
+    @package5 = Package.find(7)
+    @package5offer = Offer.find("8h58ag")
+    @package6 = Package.find(8)
+    @package6offer = Offer.find("mct5zg")
+    @package7 = Package.find(9)
+    @package7offer = Offer.find("qybczg")
+    @package8 = Package.find(10)
+    @package8offer = Offer.find("47r5qg")
 
     respond_to do |format|
       format.html # index.html.erb
