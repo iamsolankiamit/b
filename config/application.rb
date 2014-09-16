ad9633d0c8a32b8cd73393f577aef2ec0b7ff559
Amit Solanki <amit.fash@gmail.com>
2014-09-16 15:03:38 +0530

    ..

--- config/application.rb
+++ config/application.rb
@@ -64,6 +64,6 @@ module Roomnhouse
     config.assets.enabled = true
     config.assets.digest = true
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '2.0'
   end
 end
