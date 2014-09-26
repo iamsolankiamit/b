b48caf69dae5282596821b58a2265640eb00e524
Amit Solanki <amit.fash@gmail.com>
2014-09-26 10:08:15 +0530

    ..

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,6 @@ Roomnhouse::Application.configure do
   config.static_cache_control = "public, max-age=2592000"
   config.action_controller.perform_caching = true
 
-  config.eager_load = true
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
@@ -35,7 +34,7 @@ Roomnhouse::Application.configure do
   config.assets.css_compressor = :yui
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
