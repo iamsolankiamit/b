68712a85fc57aad35634177f32e823bf2024083a
Amit Solanki <amit.fash@gmail.com>
2014-10-16 20:05:43 +0530

    ..

--- app/models/referal_email.rb
+++ app/models/referal_email.rb
@@ -10,10 +10,10 @@ def self.add_manual_contacts (referer, email)
 	referral.save!
 	add_to_mailing_list(referral)
 end
-def self.add_gmail_contacts (referer, email, &other_details)
+def self.add_gmail_contacts (referer, email, *other_details)
 	referral = ReferalEmail.new(:referer_id => referer, :emails => email)
-	referral.first_name = other_details[:first_name] if other_details[:first_name]
-	referral.last_name = other_details[:last_name] if other_details[:last_name]
+	referral.first_name = other_details[0] if other_details[0]
+	referral.last_name = other_details[1] if other_details[1]
 	referral.save!
 	add_to_mailing_list(referral)
 end
