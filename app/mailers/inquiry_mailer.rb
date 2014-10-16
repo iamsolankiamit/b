5fbc35703dbbce88aee446203793c86879855c91
Amit Solanki <amit.fash@gmail.com>
2014-10-16 20:51:44 +0530

    ...

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,8 +1,8 @@
 class InquiryMailer < ActionMailer::Base
   default from: "notifications@roomnhouse.com"
-  def new_inquiry(inquiry)
+  def new_inquiry(inquiry,host_id)
     @inquiry = inquiry
-    @host = User.find(inquiry.host_id)
+    @host = User.find(host_id)
     @offer = Offer.find(inquiry.offer_id)
     mail(:to => user.email, :subject => "Booking complete")
   end
