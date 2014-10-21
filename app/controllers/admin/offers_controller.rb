c27dfb49c2d1bf97c25cd7ac69a657dc5aec29e1
Amit Solanki <amit.fash@gmail.com>
2014-10-21 13:15:25 +0530

    ..

--- app/controllers/admin/offers_controller.rb
+++ app/controllers/admin/offers_controller.rb
@@ -28,12 +28,22 @@ class Admin::OffersController < ApplicationController
   end
 
   def create
+    @offer.translations.build(params[:id])
+    @offer.build_amenity(params[:id])
     @offer = Offer.create!(offer_params)
     redirect_to admin_user_offer_photos_path(@offer.user_id,@offer.id)
   end
 
   def update
-    @offer = Offer.update!(offer,offer_params)
+    @offer = Offer.find(params[:id])
+    if !@offer.amenity
+      @amenity = @offer.build_amenity
+      @amenity = @offer.create_amenity(params[:amenities])
+    end
+    if !@offer.translations
+      @translations = @offer.create_translations(params[:translations])
+    end
+    @offer = @offer.update_attributes(offer,offer_params)
     redirect_to admin_user_offer_path(@offer.user_id, @offer.id)
   end
 
