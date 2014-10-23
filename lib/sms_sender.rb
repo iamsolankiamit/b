99ad82e599c1a8400f3ab939cee5a88feaff2a5e
Amit Solanki <amit.fash@gmail.com>
2014-10-23 18:21:59 +0530

    ...

--- lib/sms_sender.rb
+++ lib/sms_sender.rb
@@ -1,45 +1,41 @@
 require 'net/http'
 require 'uri'
+require "cgi"
 class SmsSender
 
 
   def initialize(guest,host,trip,booking)
     guest_booking_done_sms(guest,trip,booking)
     host_new_booking(guest,host,trip,booking)
-    
   end
 
   def guest_booking_done_sms(guest, trip,booking)
-    str1 = "<?xml version='1.0' encoding='iso-8859-1'?>
-<smslist>
- <sms>
-  <user>rnhcom</user>
-  <password>aruhat123</password>
-  <message>Your Booking request has been made. Confirmation from host is remaining. Thanks for using roomnhouse</message>
-  <mobiles>09869623821</mobiles>
-  <accountusagetypeid>1</accountusagetypeid>
- </sms>
-</smslist>"
-    uri = URI("http://www.vibgyortel.in/apps/sendsms.jsp")
-    http = Net::HTTP.new(uri.host,uri.port)
-    response = http.post(uri.path,str1, initheader = {"Content-Type" => "application/xml"})
+    message = "Your Booking request has been made you will be notified within 24hrs."
+    pass="RakeshAmit@1"
+    senderId= "RNHCOM"
+    mobile = 9638462979
+    to = guest.phone
+    domain = "http://trans.boommail.in"
+    path = "/SmsStatuswithId.aspx"
+    params = {mobile: mobile,pass: pass,senderid: senderId, to: to, msg: message}
+    http_get(domain,path,params)
+  end
+
+  def http_get(domain,path,params)
+    return Net::HTTP.get(domain, "#{path}?".concat(params.collect { |k,v| "#{k}=#{CGI::escape(v.to_s)}" }.join('&'))) if not params.nil?
+    return Net::HTTP.get(domain, path)
   end
 
   def host_new_booking(guest,host, trip, booking)
-    str1 = "<?xml version='1.0' encoding='iso-8859-1'?>
-<smslist>
- <sms>
-  <user>rnhcom</user>
-  <password>aruhat123</password>
-  <message>You have new booking check it out on roomnhouse.com</message>
-  <mobiles>09869623821</mobiles>
-  <accountusagetypeid>1</accountusagetypeid>
- </sms>
-</smslist>"
-    puts str1
-    uri = URI("http://www.vibgyortel.in/apps/sendsms.jsp")
-    http = Net::HTTP.new(uri.host,uri.port)
-    response = http.post(uri.path,str1, initheader = {"Content-Type" => "application/xml"})
+    message = "You have new booking check it on roomnhouse.com"
+    pass="RakeshAmit@1"
+    senderId= "RNHCOM"
+    mobile = 9638462979
+    to = host.phone
+    domain = "http://trans.boommail.in"
+    path = "/SmsStatuswithId.aspx"
+    params = {mobile: mobile,pass: pass,senderid: senderId, to: to, msg: message}
+    http_get(domain,path,params)
   end
 end
 
