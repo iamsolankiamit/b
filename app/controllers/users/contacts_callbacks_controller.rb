17dea5ba56b79361c72b7f2b5919323ea4131576
Amit Solanki <amit.fash@gmail.com>
2014-10-31 13:51:45 +0530

    ..

--- app/controllers/users/contacts_callbacks_controller.rb
+++ app/controllers/users/contacts_callbacks_controller.rb
@@ -3,7 +3,7 @@ class Users::ContactsCallbacksController < ApplicationController
   def importer
     @contacts = request.env['omnicontacts.contacts']
     @contacts.each do |contact|
-      ReferalEmail.add_gmail_contacts(current_user.id, contact[:email], contact[:first_name], contact[:last_name])
+      ReferalEmail.add_gmail_contacts(1, contact[:email], contact[:first_name], contact[:last_name])
     end
     redirect_to '/i', :notice => "gmail contacts have been referred"
   end
