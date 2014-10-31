8fb049c33844e4856e4845e49a98c79ca27dc45c
Amit Solanki <amit.fash@gmail.com>
2014-10-31 13:57:09 +0530

    ..

--- app/controllers/users/contacts_callbacks_controller.rb
+++ app/controllers/users/contacts_callbacks_controller.rb
@@ -3,8 +3,8 @@ class Users::ContactsCallbacksController < ApplicationController
   def importer
     @contacts = request.env['omnicontacts.contacts']
     @contacts.each do |contact|
-      ReferalEmail.add_gmail_contacts(1, contact[:email], contact[:first_name], contact[:last_name])
+      ReferalEmail.add_gmail_contacts(1, contact[:email])
     end
-    redirect_to '/i', :notice => "gmail contacts have been referred"
+    redirect_to '/contest', :notice => "Gmail Contacts have been refered, share and tweet to increase your chance to win."
   end
 end
--- app/models/referal_email.rb
+++ app/models/referal_email.rb
@@ -10,12 +10,10 @@ def self.add_manual_contacts (referer, email)
 	referral.save!
 	add_to_mailing_list(referral)
 end
-def self.add_gmail_contacts (referer, email, *other_details)
+def self.add_gmail_contacts (referer, email)
 	referral = ReferalEmail.new(:referer_id => referer, :emails => email)
-	referral.first_name = other_details[0] if other_details[0]
-	referral.last_name = other_details[1] if other_details[1]
 	referral.save!
-	add_to_mailing_list(referral)
+#	add_to_mailing_list(referral)
 end
 
 def self.add_to_mailing_list(referral)
