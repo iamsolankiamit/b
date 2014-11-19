04abae144d12698f5ff0db8d3df19e82b3fe2415
Amit Solanki <amit.fash@gmail.com>
2014-11-19 11:56:21 +0530

    ..

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
 		@offers = Offer.where(visiblity: true, is_verified: true).order("RANDOM()").limit(4).includes(:translations,:photos)
-    @recently_viewed_offers = Offer.where(id: session[:recently_viewed_offers]).limit(4) if session[:recently_viewed_offers]
+    @recently_viewed_offers = Offer.where(id: session[:recently_viewed_offers], visiblity: true, is_verified: true).includes(:translations,:photos).limit(4) if session[:recently_viewed_offers]
   end
 end
