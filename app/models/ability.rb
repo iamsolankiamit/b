2945b8e5e2b460a60dae23a6dfc7ef01ac00f620
Amit Solanki <amit.fash@gmail.com>
2014-12-17 16:37:39 +0530

    ..

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -39,15 +39,16 @@ else
       can :manage, Admin
     else
       can :read, [Offer,User,Booking] # guest user
+      can :email, Booking
       can :create, [Offer, Booking]
     end
 end
-    # The first argument to `can` is the action you are giving the user 
+    # The first argument to `can` is the action you are giving the user
     # permission to do.
     # If you pass :manage it will apply to every action. Other common actions
     # here are :read, :create, :update and :destroy.
     #
-    # The second argument is the resource the user can perform the action on. 
+    # The second argument is the resource the user can perform the action on.
     # If you pass :all it will apply to every resource. Otherwise pass a Ruby
     # class of the resource.
     #
